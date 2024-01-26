5f44018cb428ac3ef032bcda003575cf9ea5db11
Victor <viktornobe@gmail.com>
2024-01-26 12:17:21 +0000

    env variables

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-// server used to send send emails
+// server used to send emails
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT;
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(PORT, () => console.log("Server Running"));
 require('dotenv').config();
 
 const contactEmail = nodemailer.createTransport({
